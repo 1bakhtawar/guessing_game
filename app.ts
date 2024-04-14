7e59e64383e6e7bf7b0b6108758740144174e6f8
Bakhtawar Fatima <165771736+1bakhtawar@users.noreply.github.com>
2024-04-14 21:41:33 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+console.log("Guess a number between 1 to 10");
+
+console.log("Welcome to number guessing game");
+
+//now variable create
+
+
+let a = Math.floor(Math.random() * 10);
+while (true) {
+    let input = await inquirer.prompt({ name: "guess", type: "number",
+        message: "Enter your guess number between 1 to 10" 
+    });
+
+    //provide code 
+
+    let ans = input.guess;
+    if (a == ans) {
+        console.log("congratulation your guess number absolutely correct");
+        break;
+    }
+    else {
+        console.log("Sorry you guess wrong number try again ");
+    }
+}
